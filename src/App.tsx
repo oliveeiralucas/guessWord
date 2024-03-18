3bdb9606faeb634e445799ffdcf9d10d9bd47bb8
oliveeiralucas <lucas.oliveira@edu.unifil.br>
2024-03-18 16:46:48 -0300

    chore: atualiza projeto para minhas necessidades

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import 'App.css'
+
+import { createRoot } from 'react-dom/client'
+
+import App from '@/components/Main'
+
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
+
+root.render(<App />)
